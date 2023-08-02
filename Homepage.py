4bb1e7ca8b7d6c0d2cdb0d288881786a83a36cc5
Noddeland <503319463@ge.com>
2023-08-02 14:41:29 +0200

    update

--- Homepage.py
+++ Homepage.py
@@ -2,11 +2,13 @@ import streamlit as st
 from page1 import app as page1
 from page2 import app as page2
 
+
+st.set_page_config("Data Analysis Program")
 st.title("Data Analysis Program")
-st.write('This program is meant to make it possible for employees at GE Healthcare Lindesnes to analyze data from the production t find possible connections, deviations and improvements.')
+st.write('This program is meant to make it possible for employees at GE Healthcare Lindesnes to analyze data from the production to find possible connections, deviations and improvements.')
 st.write('As of 05.08.2023 there are still plenty of possible implementations to be made, but the Clustering module and the Batch Analysis module gives a slight idea of the possibilities of such a program.')
 st.write('**Notes:**')
-st.write('The Clustering module currently only takes 1 file on .xlsx format, while the Batch Analysis module can take multiple files of either .txt or .xlsx format (not both at once). Example format for .txt files and xlsx for Batch Analysis module attached in program folder under the "example"-folder.')
+st.write('The Clustering module currently only takes 1 file on .xlsx format, while the Batch Analysis module can take multiple files of either .txt or .xlsx format (not both at once). Example format for .txt files and xlsx for Batch Analysis module attached in program folder under the "example"-folder. Be aware that the code has not been debugged thoroughly, and some errors can occur.')
 
 # Define page titles and corresponding functions
 PAGES = {
